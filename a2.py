ed9ee43648294e618c620923b796cb6f16b49d30
Erik Storm <erikbstorm@outlook.com>
2020-06-24 13:25:22 +0200

    Update a2.py

--- a2.py
+++ a2.py
@@ -3,10 +3,78 @@ import csv
 
 def main():
     data = importData("dataset.csv")
-    analyzeTime(data)
+    analyzeLanguage(data)
 
+
+def analyzeLanguage(data):
+    '''Overlap in words measured between systems.''' 
+    overlap = {'WYSIWYG': [], 'TBT': []}
+    last_gameno = 0
+    last_part = ''
+    last_interface = ''
+    last_words = []
+    words = []
+    for line in data:
+        game_no = line['GameNo']
+        part = line['Sender']
+        interface = line['INTERFACEUSED']
+        words += line['Text'].split()
+        if interface == last_interface:
+            if last_part == part:
+                pass
+            else:
+                overlap[interface].append(getOverlapWordCounts(last_words, words))
+                last_words = words
+                words = []
+        else:
+            last_interface = interface
+            last_words = words
+            words = []
+
+    print(overlap)
+    overlap = {k: sum(v)/len(v) for k, v in overlap.items()}
+    print(overlap)
+
+
+def getOverlapWordCounts(text1, text2):
+    overlap_count = 0
+    longest_text = text1 if len(text1) > len(text2) else text2
+    if longest_text == text1: shortest_text = text2
+    for word in text1:
+        if word in text2:
+            overlap_count += 1
+    return overlap_count
+
+
+def analyzeTurnTaking(data):
+    '''Analyzes the average amount of messages before the other participant intervenes.'''
+    avg_turns = {'WYSIWYG': [], 'TBT': [], '': []}
+
+    last_interface = ''
+    last_part = ''
+    count = 1
+    last_gameno = 0
+    for line in data:
+        #print(line)
+        if last_interface == line['INTERFACEUSED']:
+            if last_part == line['Sender']:
+                count += 1
+            else:
+                avg_turns[line['INTERFACEUSED']].append(count)
+                last_part = line['Sender']
+                count = 1
+        else:
+            avg_turns[line['INTERFACEUSED']].append(count)
+            last_interface = line['INTERFACEUSED']
+            count = 1
+        
+
+    print(avg_turns)
+    avg_turns = {k: sum(v)/len(v) for k, v in avg_turns.items()}
+    print(avg_turns)
 
 def analyzeTime(data):
+    '''Average time of a game between interfaces.'''
     matrix = {}
 
     for line in data:
